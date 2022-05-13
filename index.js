603552c96902bf56f175abf90886a3f735ff6453
Kristine Kochie <kristinekochie@gmail.com>
2022-05-13 17:02:59 -0400

    Thank you card creator

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+//Thank you card creator
+const messages = [];
+
+function writeCards(names, eventType) {
+    for (let i=0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the lovely ${eventType} gift.`);
+    }
+    return messages;
+}
+
+writeCards(["Kristy", "Emmett", "Frida"], "birthday");
+
+console.log(messages);
